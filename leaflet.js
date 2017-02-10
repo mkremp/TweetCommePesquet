478d2e1d431000b7d96a60e64ba731294c6a6dc3
Coline <co.hallier@gmail.com>
2017-02-10 12:25:57 +0100

    Création de fonctions

--- leaflet.js
+++ leaflet.js
@@ -1,14 +1,11 @@
 window.onload=function(){
 
-    var map = L.map('map')
+  var map = L.map('map')
 
-
-
-    L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+  L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-	
-	map.setView([51, -0.09], 13);
+  map.setView([0, 0], 2);
 
 	var iss = {
 		url: "https://api.wheretheiss.at/v1/satellites/25544",
@@ -26,21 +23,52 @@ window.onload=function(){
 	  //iconAnchor: [9, 21],
 	  //popupAnchor: [0, -14]
 	});
-	
-	var marqueurs=L.layerGroup()
+
+  var pointList = new Array();
+
+  var followISS=document.getElementById("test1");
+
+  var marqueurs=L.layerGroup()
 		.addTo(map);
-		
-	var pointList = new Array();
-	//en global ça ne marche pas :'(
-	//var polyline = new L.polyline(pointList, {color: 'red',weight: '3'});
-	//map.addLayer(polyline);
-	
-	
-	
-	
+
+  function mark(point){
+    //gestion des marqueurs
+    marqueurs.clearLayers();
+    L.marker(point, {icon:myIcon})
+    .addTo(marqueurs)
+    .bindPopup("ISS")
+    .openPopup();
+  };
+
+  function poly(long,point,pointlist){
+    if(long<=170){
+          pointlist.push(point)
+    }
+    else{
+          polyline.clearLayers();
+        }
+    var polyline = new L.polyline(pointList, {color: 'red',weight: '3'});
+    map.addLayer(polyline);
+  };
+
+function buttonFollow(button,map,point){
+  button.addEventListener('change',function(event){
+    event.preventDefault();
+    if (followISS.checked){
+        map.setZoom(7);
+        map.panTo(point);
+        console.log(point);
+        console.log(map.getCenter());
+    }else{
+        map.setView([0, 0], 2)
+
+    }
+  });
+};
+
 	ajaxIss();
 
-	
+
 
     function ajaxIss(){
       var ajax = new XMLHttpRequest();
@@ -54,32 +82,17 @@ window.onload=function(){
               var str=ajax.responseText;
               var jsonObj = JSON.parse(str);
               var result = iss.getInfos(jsonObj);
-			  //Bouton suivre
-			  var followISS=document.getElementById("test1");
-			  followISS.addEventListener('change',function(event){
-				  event.preventDefault();
-				  if (followISS.checked){
-					  map.setView([result.lat,result.long],13)
-				  }else{
-					  map.setView([51, -0.09], 2)
-					}
-				});
-              //gestion des marqueurs
-              marqueurs.clearLayers();
-              L.marker([result.lat, result.long], {icon:myIcon})
-              .addTo(marqueurs)
-              .bindPopup("ISS")
-              .openPopup();
+              //Création d'un objet LatLng
+              var point = new L.LatLng(result.lat,result.long)
+              //Bouton suivre
+              buttonFollow(followISS,map,point);
               //création de la ligne de déplacement de l'iss
-              if(result.long!=180){
-				var point = new L.LatLng(result.lat,result.long)
-				pointList.push(point)
-              }
-              else{
-				  polyline.clearLayers();
-				  }
-              var polyline = new L.polyline(pointList, {color: 'red',weight: '3'});
-              map.addLayer(polyline);
+              poly(result.long,point,pointList);
+              //gestion des marqueurs
+              mark(point);
+
+
+
               //affichage de la latitude et longitude
               document.getElementById("lat").innerHTML = "Latitude : "+result.lat;
               document.getElementById("long").innerHTML = "Longitude : "+result.long;
@@ -88,8 +101,8 @@ window.onload=function(){
               //répétition de la fonction toutes les 5 secondes
               setTimeout(function() {ajaxIss ()},5000);
 
-			}
-		});
+			    }
+		  });
       ajax.send("");
 
 
