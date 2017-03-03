06fbcaf6a24e2d2ba88d95ca1da8b5d873bd606e
mkremp <mathilde.kremp@gmail.com>
2017-03-03 09:50:05 +0100

    Add files via upload

--- leaflet.js
+++ leaflet.js
@@ -1,5 +1,6 @@
 window.onload=function(){
 
+  //affichage de la carte
   var map = L.map('map')
 
   L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
@@ -7,8 +8,10 @@ window.onload=function(){
     }).addTo(map);
   map.setView([0, 0], 2);
 
+  //variable utilisée pour faire appel à l'api de localisation de l'iss
 	var iss = {
 		url: "https://api.wheretheiss.at/v1/satellites/25544",
+    url2 :"http://127.0.0.1/Tweet/api/apiIss.php?",
 		methode: "GET",
 		getInfos: function(jsonObj){
 		  lat=jsonObj.latitude;
@@ -17,10 +20,12 @@ window.onload=function(){
 		}
 	};
 
+
+  //api de récupération des données de lieu
   var loc = {
     url1:'http://api.geonames.org/findNearbyPlaceNameJSON?', //utilisée pour la premiere api
     url2:'http://api.geonames.org/extendedFindNearby?', // idem
-    url3:'http://127.0.0.1/tcp/main.php?', // utilisée pour notre propre api
+    url3:'http://127.0.0.1/Tweet/api/main.php?', // utilisée pour notre propre api
     methode: "GET",
     getInfos: function(jsonObj){
 		if (jsonObj.geoname==null){
@@ -31,21 +36,15 @@ window.onload=function(){
 	  }else{
       cityName=jsonObj.geoname[4].toponymName;
       country=jsonObj.geoname[4].countryName;
-      //console.log(cityName);
+    }
 
-	  }
-
-      //console.log(cityName);
-      return{cityName:cityName , country:country};
-
-      }
+    return{cityName:cityName , country:country};
+    }
     };
 
-  var locIss = {
-    url4 : 'http://127.0.0.1/tcp/apiIss.php',
-    methode: "GET"
-  }
 
+
+  //style du marqueur
 	var myIcon = L.icon({
 	  iconUrl: 'navette.png',
 	  iconSize: [29, 24],
@@ -53,6 +52,7 @@ window.onload=function(){
 	  //popupAnchor: [0, -14]
 	});
 
+  //déclaration de toutes les variables globales
   var pointList = new Array();
   var point = new L.LatLng(0,0);
   var followISS=document.getElementById("checkboxFollow");
@@ -67,11 +67,14 @@ window.onload=function(){
 
   var marqueurs=L.layerGroup()
 		.addTo(map);
-/*
-var correct = function(a){
+
+
+  //fonction de récupération de résultat d'une requete ajax, mais ne fonctionne pas
+  /*
+  var correct = function(a){
     return a;
   };
-*/
+  */
 
 
 
@@ -84,6 +87,7 @@ var correct = function(a){
     .openPopup();
   };
 
+  //création de la polyligne pour suivre le déplacement de l'iss
   function poly(long,point,pointlist){
 	var polyline = new L.polyline(pointList, {color: 'red',weight: '3'});
     if(long<=178){
@@ -101,6 +105,7 @@ var correct = function(a){
     map.addLayer(polyline);
   };
 
+  //listener sur la case cochée followISS
 	followISS.addEventListener('change',function(event){
     event.preventDefault();
 		if (followISS.checked){
@@ -111,49 +116,44 @@ var correct = function(a){
 		}
 	});
 
+  //listener sur la case cochée mode débug
   debug.addEventListener('change',function(event){
     event.preventDefault();
     if(debug.checked){
       //lat et long iss = ce que l'on calcule dans le apiIss.php
+      iss.url="http://127.0.0.1/Tweet/api/apiIss.php?"
+    }else{
+      iss.url="https://api.wheretheiss.at/v1/satellites/25544"
     }
-  })
+  });
 
 
-    validation.addEventListener('click', function(event) {
-      event.preventDefault();
-        photo(long,lat,zoom);
-      picture.innerHTML = '<img src='+urlpicture+' />';
-      //console.log(resultTxt.cityName);
-      var ajax1 = new XMLHttpRequest();
-      ajax1.open(loc.methode, loc.url3+'&lat='+lat+'&lng='+long+'&username=mkremp', true);
-      //console.log("lapin");
-      // métadonnées de la requête AJAX
-      ajax1.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      ajax1.addEventListener('readystatechange',function(){
-      if(ajax1.readyState == 4 && ajax1.status == 200) {
-            caseTweet.innerHTML="";
-              // le texte de la réponse
-            var str1=ajax1.responseText;
-            console.log(str1);
-            console.log("lapin");
-
-            var jsonObj1 = JSON.parse(str1);
-            console.log(jsonObj1);
-            var result1 = loc.getInfos(jsonObj1);
-            //console.log(result1);
-
-            caseTweet.innerHTML+="Hello " + result1.cityName + " - " + result1.country+" !" ;
-          }
-
-        });
-        ajax1.send("");
-
-
+  validation.addEventListener('click', function(event) {
+    event.preventDefault();
+    photo(long,lat,zoom);
+    picture.innerHTML = '<img src='+urlpicture+' />';
+    var ajax1 = new XMLHttpRequest();
+    ajax1.open(loc.methode, loc.url3+'&lat='+lat+'&lng='+long+'&username=mkremp', true);
+    // métadonnées de la requête AJAX
+    ajax1.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    ajax1.addEventListener('readystatechange',function(){
+    if(ajax1.readyState == 4 && ajax1.status == 200) {
+      caseTweet.innerHTML="";
+      // le texte de la réponse
+      var str1=ajax1.responseText;
+      console.log(str1);
+      var jsonObj1 = JSON.parse(str1);
+      console.log(jsonObj1);
+      var result1 = loc.getInfos(jsonObj1);
+      caseTweet.innerHTML+="Hello " + result1.cityName + " - " + result1.country+" !" ;
+      }
       });
+      ajax1.send("");
+  });
 
 
 
-//premier cas : première api qui ne donne pas le nom de l'océan..
+  //premier cas : première api qui ne donne pas le nom de l'océan..
   /*validation.addEventListener('click', function(event) {
     event.preventDefault();
 	  photo(long,lat,zoom);
@@ -173,37 +173,31 @@ var correct = function(a){
           console.log(result1);
           caseTweet.innerHTML+="Hello " + result1.cityName + " - " + result1.country+" !" ;
         }
-
-      });
+        });
       ajax1.send("");
-
-
   });*/
 
 
 
 
 
-
+  //gestion des boutons de zoom
 	function center (map,point,radio) {
 		for (i=0;i<radio.length;i++){
 			if (radio[i].checked){
 					map.flyTo(point,radio[i].value);
 				}
-		//console.log(point);
-		//console.log(map.getCenter());
 		}
 	};
 
+
+  //génération de la photo 
 	function photo(long,lat,zoom){
 
         for (i=0;i<zoom.length;i++){
           	if (zoom[i].checked){
-
-				//console.log(zoom[i].value);
-				urlpicture="https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+long+","+lat+","+zoom[i].value+'/700x250?access_token=pk.eyJ1IjoiY29oYWxsaWVyIiwiYSI6ImNpemVmODgxZTAwNzgzMnBlZzRkMXh1MjcifQ.0EiwSBDZMzgfEcam2M6nUA';
-			}
-
+				      urlpicture="https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+long+","+lat+","+zoom[i].value+'/700x250?access_token=pk.eyJ1IjoiY29oYWxsaWVyIiwiYSI6ImNpemVmODgxZTAwNzgzMnBlZzRkMXh1MjcifQ.0EiwSBDZMzgfEcam2M6nUA';
+			       }
         };
 		return (urlpicture)
 	}
@@ -217,86 +211,61 @@ var correct = function(a){
       ajax1.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
       ajax1.addEventListener('readystatechange',function(){
 		  if(ajax1.readyState == 4 && ajax1.status == 200) {
-              // le texte de la réponse
-            var str1=ajax1.responseText;
-            var jsonObj1 = JSON.parse(str1);
-            var result1 = loc.getInfos(jsonObj1);
-            console.log(result1);
-            return{result1:result1};
-
+        // le texte de la réponse
+        var str1=ajax1.responseText;
+        var jsonObj1 = JSON.parse(str1);
+        var result1 = loc.getInfos(jsonObj1);
+        return{result1:result1};
       }
 
     });
     ajax1.send("");
+  };*/
+
+  ajaxIss();
 
 
-	};*/
-
-
-
-
-	ajaxIss();
-
-
-
-    function ajaxIss(){
-      var ajax = new XMLHttpRequest();
-      ajax.open(iss.methode, iss.url, true);
-      // métadonnées de la requête AJAX
-      ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      ajax.addEventListener('readystatechange',function(){
-        // si l'état est le numéro 4 et que la ressource est trouvée
-          if(ajax.readyState == 4 && ajax.status == 200) {
-              // le texte de la réponse
-            var str=ajax.responseText;
-            var jsonObj = JSON.parse(str);
-            var result = iss.getInfos(jsonObj);
-            //Création d'un objet LatLng
-            point = new L.LatLng(result.lat,result.long);
-            //Bouton suivre
+  //requete ajax pour localiser l'iss, tracer la polyligne
+  function ajaxIss(){
+    var ajax = new XMLHttpRequest();
+    ajax.open(iss.methode, iss.url, true);
+    // métadonnées de la requête AJAX
+    ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    ajax.addEventListener('readystatechange',function(){
+    // si l'état est le numéro 4 et que la ressource est trouvée
+    if(ajax.readyState == 4 && ajax.status == 200) {
+      // le texte de la réponse
+      var str=ajax.responseText;
+      var jsonObj = JSON.parse(str);
+      var result = iss.getInfos(jsonObj);
+      //Création d'un objet LatLng
+      point = new L.LatLng(result.lat,result.long);
+      //Bouton suivre
 			if (followISS.checked){
 				//console.log(zoom[0].value);
 				center(map,point,zoom);
 			}
 
-              //création de la ligne de déplacement de l'iss
-              poly(result.long,point,pointList);
-              //gestion des marqueurs
-              mark(point);
+      //création de la ligne de déplacement de l'iss
+      poly(result.long,point,pointList);
+      //gestion des marqueurs
+      mark(point);
 
+      //affichage de la latitude et longitude
+      document.getElementById("lat").innerHTML = 'Latitude : ' +result.lat;
+      document.getElementById("long").innerHTML ='Longitude : '+result.long;
 
+      //generation de l'url de la photo au cas où un user souhaite tweeter
+      urlpicture=photo(result.long,result.lat,radio);
 
-              //affichage de la latitude et longitude
-              document.getElementById("lat").innerHTML = 'Latitude : ' +result.lat;
-              document.getElementById("long").innerHTML ='Longitude : '+result.long;
+      //répétition de la fonction toutes les 5 secondes
+      setTimeout(function() {ajaxIss ()},5000);
 
-              //generation de l'url de la photo au cas où un user souhaite tweeter
-              urlpicture=photo(result.long,result.lat,radio);
+      }
+			    
+    });
+    ajax.send("");
+		};
 
+};
 
-
-
-
-
-
-              //generation de l'url de recherche du lieu de la photo
-              //recherche ='&lat='+result.lat+'&lng='+result.long+'&username=mkremp' ;
-
-
-
-
-
-
-              //répétition de la fonction toutes les 5 secondes
-              setTimeout(function() {ajaxIss ()},5000);
-
-            }
-			    });
-          ajax.send("");
-		  };
-
-
-
-
-
-    };
